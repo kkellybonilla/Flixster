160023791408c989bd238b4129ecf6ce695746ac
kkellybonilla <kkelly.bonilla@stanford.edu>
2023-03-06 01:44:44 -0500

    Initial Commit

---
+++ project2-FlixsterPt1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project2-FlixsterPt1
+//
+//  Created by Kelly Bonilla Guzmán on 3/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
