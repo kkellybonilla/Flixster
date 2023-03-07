c7b15c7390717a35e0c4ce0287b3bc1c4d812fe8
kkellybonilla <kkelly.bonilla@stanford.edu>
2023-03-06 23:21:26 -0500

    Implemented Scroll View

--- project2-FlixsterPt1/DetailViewController.swift
+++ project2-FlixsterPt1/DetailViewController.swift
@@ -10,6 +10,12 @@ import Nuke
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var backdropImageView: UIImageView!
+    @IBOutlet weak var movieDetailTitleLabel: UILabel!
+    @IBOutlet weak var movieDetailDescriptionLabel: UILabel!
+    @IBOutlet weak var voteAverageLabel: UILabel!
+    @IBOutlet weak var voteCountLabel: UILabel!
+    @IBOutlet weak var popularityLabel: UILabel!
     
     // A property to store the movie object.
     // We can set this property by passing along the movie object associated with the movie the user tapped in the table view.
@@ -18,6 +24,14 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Load the image located at the `backdropImageView` URL and set it on the image view.
+        Nuke.loadImage(with: movie.backdropImage, into: backdropImageView)
+        
+        movieDetailTitleLabel.text = movie.title
+        movieDetailDescriptionLabel.text = movie.overview
+        voteAverageLabel.text = String(movie.voteAverage) + " Vote Average"
+        voteCountLabel.text = String(movie.voteCount) + " Votes"
+        popularityLabel.text = String(movie.popularity) + " Popularity"
     }
     
 
